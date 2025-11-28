e6784d38db260856b5191d31716c91c50a9bf1c4
roshanchevli <chevlir115@gmail.com>
2025-11-28 17:31:07 +0530

    fix crash error

--- app/src/main/java/com/example/firebasedemo/MainActivity.kt
+++ app/src/main/java/com/example/firebasedemo/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.firebasedemo
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.firebasedemo.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,15 +17,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener((binding.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        binding.button.setOnClickListener {
-
+        binding.tvname.text = FirebaseAuth.getInstance().currentUser?.displayName
+        binding.tvemail.text = FirebaseAuth.getInstance().currentUser?.email
+        binding.btnlogout.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            val intent = Intent(this, login::class.java)
+            startActivity(intent)
         }
     }
 }
